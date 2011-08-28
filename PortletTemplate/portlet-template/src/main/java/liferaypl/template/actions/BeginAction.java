cfc1dacd7e086de712d08627f7b3fe620c74426d
Jeferson Leao <jsanchesleao@gmail.com>
2011-08-28 13:09:20 -0300

    Included the PortletTemplate skeleton files

---
+++ PortletTemplate/portlet-template/src/main/java/liferaypl/template/actions/BeginAction.java
@@ -0,0 +1,11 @@
+package liferaypl.template.actions;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class BeginAction {
+
+	public String execute(){
+		return ActionSupport.SUCCESS;
+	}
+	
+}
