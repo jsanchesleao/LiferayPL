1c51eb2b0c57b9ae55f24cb8fdd0e83ae0f5f9f4
Fernando Correa de Oliveira <fernandocorrea@gmail.com>
2011-09-01 19:40:57 -0300

    adcionada a feature de log

--- PerlApi/Flanelinha-Client/lib/Flanelinha/Client.pm
+++ PerlApi/Flanelinha-Client/lib/Flanelinha/Client.pm
@@ -5,6 +5,15 @@ use Mojo::UserAgent;
 has ua        => (is => "rw", isa => "Mojo::UserAgent", default => sub{Mojo::UserAgent->new});
 has url       => (is => "rw", isa => "Str", default => "http://localhost:3000");
 has namespace => (is => "rw", isa => "Str", required => 1);
+has "log"     => (is => "ro", isa => "Flanelinha::Client::Log", lazy => 1,
+                  default => sub {
+                     my $self = shift;
+                     Flanelinha::Client::Log->new(
+                        ua        => $self->ua,
+                        url       => $self->url,
+                        namespace => $self->namespace,
+                     )
+                  });
 
 sub portlet {
    my $self = shift;
@@ -17,11 +26,46 @@ sub portlet {
    }
 }
 
-sub log {
-   my $self = shift;
-   my $msg  = shift;
+package Flanelinha::Client::Log;
+use Moose;
+
+extends "Flanelinha::Client";
+
+has "+ua"         => (required => 1);
+has "+url"        => (required => 1);
+has "+namespace"  => (required => 1);
+
+override log => sub{
+   die "You cannot use log of a log"
+};
+
+sub send_log {
+   my $self  = shift;
+   my $level = shift;
+   my $msg   = shift;
    my $namespace = $self->namespace;
-   return $self->ua->post_form($self->url . "/portlet/$namespace/log" => {msg => $msg})->res->code == 200
+   return $self->ua->post_form($self->url . "/portlet/$namespace/log" => {level => $level, msg => $msg})->res->code == 200
 }
+
+sub trace {
+   shift()->send_log("TRACE" => shift);
+}
+
+sub info {
+   shift()->send_log("INFO" => shift);
+}
+
+sub debug {
+   shift()->send_log("DEBUG" => shift);
+}
+
+sub warn {
+   shift()->send_log("WARN" => shift);
+}
+
+sub error {
+   shift()->send_log("ERROR" => shift);
+}
+
 
 42
--- PerlApi/flanelinha.pl
+++ PerlApi/flanelinha.pl
@@ -3,6 +3,15 @@
 use Mojolicious::Lite;
 
 my %portlet;
+my @logs;
+
+my %log_dic = (
+   TRACE => 5,
+   DEBUG => 4,
+   INFO  => 3,
+   WARN  => 2,
+   ERROR => 1,
+);
 
 under "/portlet/:namespace" => sub {
    my $self = shift;
@@ -37,9 +46,39 @@ del "/" => sub{
 
 post "/log" => sub{
    my $self = shift;
-   my $msg = $self->param("msg");
-   app->log->debug($self->stash->{portlet}->{namespace} . ": " . $msg);
+   my $level = $self->param("level");
+   my $msg   = $self->param("msg");
+   push @logs, {level => $level, msg => $msg};
+   app->log->log($level, $self->stash->{namespace} . ": $msg");
    $self->render_json(1)
 } => undef;
+
+get "/log" => sub{
+   my $self = shift;
+   my @logs_local = @logs;
+   @logs = ();
+   $self->render_json(\@logs_local)
+};
+
+get "/log/:loglevel" => sub{
+   my $self = shift;
+   app->log->debug("Log level: " . $self->stash->{loglevel});
+   my @logs_local = grep {
+      app->log->debug(" -> " . $_->{level});
+      $log_dic{$_->{level}} <= $log_dic{$self->stash->{loglevel}}
+   } @logs;
+   @logs = ();
+   $self->render_json(\@logs_local)
+};
+
+websocket "/logreader/:loglevel" => sub{
+   my $self = shift;
+   app->log->debug("websocket conectado!");
+   $self->on_message(sub{
+      my ($self, $message) = @_;
+      $self->send_message($message);
+      app->log->debug("ws: " . $message);
+   });
+};
 
 app->start;
